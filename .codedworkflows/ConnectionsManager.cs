392ec637d8f2bb72673d277ea5a05d6c9abaf94d
Shubham Mourya <shubham.mourya@testpoint.com.au>
2024-07-16 18:38:40 +0530

    Added Vansah Integration

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace uipathwithvansah
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
